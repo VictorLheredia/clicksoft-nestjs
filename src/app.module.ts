0a9439c6078df423ca49ea15125990c4dcf697a9
VictorLHeredia <victorluizheredia@gmail.com>
2022-05-15 04:53:05 -0300

    create resource Student / Teacher / Room

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { StudentModule } from './module/student/student.module';
+import { TeacherModule } from './module/teacher/teacher.module';
+import { RoomModule } from './module/room/room.module';
 
 @Module({
-  imports: [],
+  imports: [StudentModule, TeacherModule, RoomModule],
   controllers: [],
   providers: [],
 })
