4870ccb94116a94f3a89d70338ec5d1cb5d1e035
VictorLHeredia <victorluizheredia@gmail.com>
2022-05-15 05:15:34 -0300

    create Crud Student

--- src/module/student/student.controller.ts
+++ src/module/student/student.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { StudentService } from './student.service';
 
 @Controller('student')
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
+
+  @Get()
+  async findAll() {
+    return this.studentService.findAll();
+  }
+
+  @Get(':register')
+  async findOne(@Param('register') register: string) {
+    return this.studentService.findOne(+register);
+  }
+
+  @Post()
+  async create(@Body() data: Prisma.StudentCreateInput) {
+    return this.studentService.create(data);
+  }
+
+  @Patch(':register')
+  async update(
+    @Param('register') register: string,
+    @Body() data: Prisma.StudentUpdateInput,
+  ) {
+    return this.studentService.update(+register, data);
+  }
+
+  @Delete(':register')
+  async delete(@Param('register') register: string) {
+    return this.studentService.delete(+register);
+  }
 }
--- src/module/student/student.service.ts
+++ src/module/student/student.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll() {
+    return this.prisma.student.findMany();
+  }
+
+  async findOne(register: number) {
+    return this.prisma.student.findUnique({
+      where: { register: register },
+      include: { rooms: true },
+    });
+  }
+
+  async create(data: Prisma.StudentCreateInput) {
+    return this.prisma.student.create({ data });
+  }
+
+  async update(register: number, data: Prisma.StudentUpdateInput) {
+    return this.prisma.student.update({ data, where: { register: register } });
+  }
+
+  async delete(register: number) {
+    return this.prisma.student.delete({ where: { register: register } });
+  }
+}
